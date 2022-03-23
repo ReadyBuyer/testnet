4d12dd6d40b78a1f63a85d18a8661fff53e88b4d
Peter Nortey <ecexpay@gmail.com>
2022-03-23 14:01:09 +0000

    set default stake

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -14,16 +14,16 @@
 
 import { Context, logging, storage } from 'near-sdk-as'
 
-const DEFAULT_MESSAGE = 'Hello'
+const DEFAULT_STAKE = 1;
 
 // Exported functions will be part of the public interface for your smart contract.
 // Feel free to extract behavior to non-exported functions!
-export function getStake(accountId: string): string | null {
+export function getStake(accountId: string): number | null {
   // This uses raw `storage.get`, a low-level way to interact with on-chain
   // storage for simple contracts.
   // If you have something more complex, check out persistent collections:
   // https://docs.near.org/docs/concepts/data-storage#assemblyscript-collection-types
-  return storage.get<string>(accountId, DEFAULT_MESSAGE)
+  return storage.get<number>(accountId, DEFAULT_STAKE)
 }
 
 export function setStake(amount: number): void {
