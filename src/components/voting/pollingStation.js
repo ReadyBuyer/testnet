1d2b080647724be88ff03707ccc48d2a44dc0a47
Peter Nortey <ecexpay@gmail.com>
2022-03-24 02:53:09 +0000

    new poll form

--- src/components/voting/pollingStation.js
+++ src/components/voting/pollingStation.js
@@ -1,36 +1,7 @@
 import React from "react";
 
 const PollingStation = (props) => {
-  return (
-    <div>
-      <table class="table-auto">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Poll Name</th>
-            <th>Go to Poll</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-            <td>Malcolm Lockyer</td>
-            <td>1961</td>
-          </tr>
-          <tr>
-            <td>Witchy Woman</td>
-            <td>The Eagles</td>
-            <td>1972</td>
-          </tr>
-          <tr>
-            <td>Shining Star</td>
-            <td>Earth, Wind, and Fire</td>
-            <td>1975</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+  return <div></div>;
 };
 
 pollingStation.propTypes = {};
